733398f65a0da0ac2b65634d8db5e3eb6b0a1019
Jorge Burgos Ortega <114398151+J3Burgos@users.noreply.github.com>
2024-10-31 12:02:15 +0100

    Add files via upload

---
+++ SSII-main/SSII-main/src/Const.py
@@ -0,0 +1,20 @@
+
+ruta_test1_original="../Test/Tarea 1/test_map_original.txt"
+ruta_test1_300="../Test/Tarea 1/test_map_300_mean.txt"
+ruta_test1_400="../Test/Tarea 1/test_map_400_max.txt"
+ruta_test2_300="../Test/Tarea 2/sucesores_300_mean.txt"
+ruta_test2_400="../Test/Tarea 2/sucesores_400_max.txt"
+ruta_test3="../Test/Tarea 3/"
+ruta_test4="../Test/Tarea 4/"
+
+FileNameGlobal="../../SSDD/LaGomera.hdf5"
+DirNameResize="Maps_Resize/"
+
+H_EUCLIDIANA = 0
+H_MANHATTAN = 1
+
+BFS = 2
+DFS = 3
+UCS = 4
+VORAZ=5
+A=6
